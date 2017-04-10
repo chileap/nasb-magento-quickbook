dc7010f6b7b416d946eaff072c910f5b802c1501
chileap <chileapchhin@gmail.com>
2017-04-10 16:42:29 +0700

    refactor tax script push from production to staging

--- app/services/quickbooks_taxes.rb
+++ app/services/quickbooks_taxes.rb
@@ -64,6 +64,17 @@ class QuickbooksTaxes
     tax_rate = { tax_rate: tax_rate_detail, tax_agency: tax_agency_detail }
   end
 
+  def get_tax_rate(tax_rate_list)
+    tax_rates = {}
+    tax_rate_list.each do |tax_rate|
+      tax_rate_id = tax_rate.tax_rate_ref["value"]
+      tax_rate_name = tax_rate.tax_rate_ref["name"].gsub("\"", '')
+      tax_rate_detail = get_tax_rate_details(tax_rate_id)
+      tax_rates.merge!({"#{tax_rate_name}": tax_rate_detail})
+    end
+    tax_rates
+  end
+
   def check_agency_name(authentication_data, old_access_token, tax_codes_collection)
     get_access_token(authentication_data, old_access_token)
     tax_agency_service
@@ -71,28 +82,19 @@ class QuickbooksTaxes
     production_tax_codes_collection = {}
     tax_codes_collection.each do |tax_code|
       tax_code_name = tax_code.name
-      sales_tax_rates = {}
-      purchase_tax_rates = {}
       if tax_code.sales_tax_rate_list.present?
-        sales_tax_rate_details = tax_code.sales_tax_rate_list["tax_rate_detail"]
-        sales_tax_rate_details.each do |sales_tax_rate|
-          tax_rate_id = sales_tax_rate.tax_rate_ref["value"]
-          tax_rate_name = sales_tax_rate.tax_rate_ref["name"].gsub("\"", '')
-          sales_tax_rate_detail = get_tax_rate_details(tax_rate_id)
-          sales_tax_rates.merge!({"#{tax_rate_name}": sales_tax_rate_detail})
-        end
+        sales_tax_rates = get_tax_rate(tax_code.sales_tax_rate_list["tax_rate_detail"])
       end
       if tax_code.purchase_tax_rate_list.present?
-        purchase_tax_rate_details = tax_code.purchase_tax_rate_list["tax_rate_detail"]
-        purchase_tax_rate_details.each do |purchase_tax_rate|
-          tax_rate_id = purchase_tax_rate.tax_rate_ref["value"]
-          tax_rate_name = purchase_tax_rate.tax_rate_ref["name"].gsub("\"", '')
-          purchase_tax_rate_detail= get_tax_rate_details(tax_rate_id)
-          purchase_tax_rates.merge!({"#{tax_rate_name}": purchase_tax_rate_detail})
-        end
+        purchase_tax_rates = get_tax_rate(tax_code.purchase_tax_rate_list["tax_rate_detail"])
       end
       puts tax_code_name
-      production_tax_code = { name: tax_code_name, description: tax_code.description, sales_tax_rate_details: sales_tax_rates, purchase_tax_rate_details: purchase_tax_rates }
+      production_tax_code = {
+        name: tax_code_name,
+        description: tax_code.description,
+        sales_tax_rate_details: sales_tax_rates,
+        purchase_tax_rate_details: purchase_tax_rates
+      }
       production_tax_codes_collection.merge!({"#{tax_code_name}": production_tax_code})
     end
     production_tax_codes_collection
@@ -116,6 +118,18 @@ class QuickbooksTaxes
     end
   end
 
+  def tax_code_params(tax_rate_details, type)
+    tax_agency = check_agency_name_in_staging(tax_rate_details[:tax_rate].name, tax_rate_details[:tax_agency].display_name)
+    tax_code_params = {
+      "TaxRateName": "#{tax_rate_details[:tax_rate].name}",
+      "RateValue": tax_rate_details[:tax_rate].rate_value,
+      "TaxAgencyId": tax_agency.id,
+      "TaxApplicableOn": type
+    }
+    puts "#{tax_agency.id} #{tax_agency.display_name}"
+    tax_code_params
+  end
+
   def push_production_taxes_to_staging(authentication_data, old_access_token, tax_codes_with_agencies)
     get_access_token(authentication_data, old_access_token)
     query_end_point   = "#{BASE_URL}/#{@token.company_id}"
@@ -126,35 +140,23 @@ class QuickbooksTaxes
         tax_rate_details = []
         if tax_code[:sales_tax_rate_details].present?
           tax_code[:sales_tax_rate_details].each do |key, sales_tax_rate|
-            tax_agency = check_agency_name_in_staging(sales_tax_rate[:tax_rate].name, sales_tax_rate[:tax_agency].display_name)
-            sales_tax_rate = {
-              "TaxRateName": "#{sales_tax_rate[:tax_rate].name}",
-              "RateValue": sales_tax_rate[:tax_rate].rate_value,
-              "TaxAgencyId": tax_agency.id,
-              "TaxApplicableOn": "Sales"
-            }
-            puts "#{tax_agency.id} #{tax_agency.display_name}"
-            tax_rate_details << sales_tax_rate
+            sales_tax_rate_params = tax_code_params(sales_tax_rate, 'Sales')
+            tax_rate_details << sales_tax_rate_params
           end
         end
 
         if tax_code[:purchase_tax_rate_details].present?
-          tax_code[:purchase_tax_rate_details].each do |key, sales_tax_rate|
-            tax_agency = check_agency_name_in_staging(sales_tax_rate[:tax_rate].name, sales_tax_rate[:tax_agency].display_name)
-            sales_tax_rate = {
-              "TaxRateName": "#{sales_tax_rate[:tax_rate].name}",
-              "RateValue": sales_tax_rate[:tax_rate].rate_value,
-              "TaxAgencyId": tax_agency.id,
-              "TaxApplicableOn": "Purchase"
-            }
-            puts "#{tax_agency.id} #{tax_agency.display_name}"
-            tax_rate_details << sales_tax_rate
+          tax_code[:purchase_tax_rate_details].each do |key, purchase_tax_rate|
+            purchase_tax_rate_params = sales_tax_rate_params = tax_code_params(purchase_tax_rate, 'Purchase')
+            tax_rate_details << purchase_tax_rate_params
           end
         end
+
         tax_code_params = {
           "TaxCode": key,
           "TaxRateDetails": tax_rate_details
         }
+
         end_point = "#{BASE_URL}/#{@token.company_id}/taxservice/taxcode/"
         tax_code_response = @access_token.post(end_point, tax_code_params.to_json, { 'Content-Type' => 'application/json' })
         tax_code_id = JSON.parse(tax_code_response.body)["TaxCodeId"]
