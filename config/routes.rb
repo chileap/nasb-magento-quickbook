a19383388d21824b7b1fff40593b7693145b42a6
SopharyMSH <sopharydev@gmail.com>
2017-10-24 17:31:15 +0700

    add setting page with side menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
   root to: 'run#index'
+  get '/settings' => 'run#settings'
+
   resources :run do
     get :sales_receipt_report
     get :credits_memo_report
