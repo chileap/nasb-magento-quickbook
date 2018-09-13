f351401bccdbb5180f352d57461e88b81d29a416
Sila <chansila8@gmail.com>
2018-09-13 16:43:39 +0700

    test 5pm cambodia time

--- config/schedule.rb
+++ config/schedule.rb
@@ -26,90 +26,10 @@ set :output, { error: 'log/cron_error_log.log', standard: 'log/cron_log.log' }
 #   rake "magento_quickbooks_integrator:pushing_data_to_qbo"
 # end
 
-every 1.day, at: '1:00am', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '2:00am', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '3:00am', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '4:00am', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '5:00am', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
 every 1.day, at: '6:00am', roles: [:app] do
 	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
 end
 
-every 1.day, at: '7:00am', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '8:00am', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '9:00am', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
 every 1.day, at: '10:00am', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '11:00am', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '1:00pm', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '2:00pm', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '3:00pm', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '4:00pm', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '5:00pm', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '6:00pm', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '7:00pm', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '8:00pm', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '9:00pm', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '10:00pm', roles: [:app] do
-	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
-end
-
-every 1.day, at: '11:00pm', roles: [:app] do
 	rake "magento_quickbooks_integrator:pushing_data_to_qbo"
 end
